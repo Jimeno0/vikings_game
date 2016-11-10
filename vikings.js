b20cabb9faeee2f162f9866d8631c8c1795433ff
Jimeno0 <carlospj@icloud.com>
2016-11-10 16:01:19 +0100

    adding pit class and lineal game

--- vikings.js
+++ vikings.js
@@ -8,9 +8,4 @@ Viking.prototype.doAttack = function (rival) {
   rival.health -= this.strength;
 };
 
-var ragnar = new Viking('Ragnar', 100, 10);
-var floki = new Viking('Flokki', 80, 8);
-
-console.log(floki.health);
-ragnar.doAttack(floki);
-console.log(floki.health);
+module.exports = Viking;
