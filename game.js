cabaa8dce45a5cd81fef83a1db62168584c5bb8d
Jimeno0 <carlospj@icloud.com>
2016-11-10 19:07:28 +0100

    adding saxons village assault stuff

--- game.js
+++ game.js
@@ -1,14 +1,34 @@
 var Warrior = require('./warriors.js');
 var Viking = require('./warriors.js');
-// var Viking = require('./vikings.js');
 var Pit = require('./pit.js');
 
 var ragnar = new Viking(100, 10, 'Ragnar');
 var floki = new Viking(80, 8, 'Flokki');
 
-console.log(ragnar);
-console.log(floki);
+//two vikings combat
+// var pit = new Pit(ragnar,floki,10);
+// pit.startgame();
 
-var pit = new Pit(ragnar,floki,10);
+var vikings = [ragnar,floki];
+var saxons = [];
+var number_of_saxons = 4;
+for (var i = 0; i < number_of_saxons ; i++) {
+  var random_health = Math.round(Math.random()*20);
+  var random_strenght = Math.round(Math.random()*4);
+  saxons.push(new Warrior(random_health,random_strenght));
+}
+console.log('Assault beggins!');
 
-pit.startgame();
+
+while (saxons.length > 0) {
+  for (var i = 0; i < vikings.length; i++) {
+    if (saxons[i] && vikings[i]) {
+      console.log('new combat '+ vikings[i].name +' vs saxon heal ' +saxons[i].health);
+      var pit = new Pit(vikings[i],saxons[i]);
+      pit.deadCombat();
+      console.log(vikings[i].name + ' the viking with health: '+ vikings[i].health +' defeat a saxon with health: ' + saxons[i].health);
+      if (saxons[i].health <= 0) { saxons.splice(i,1);}
+    }
+  }
+}
+console.log('No more saxons, massacre ends!');
--- pit.js
+++ pit.js
@@ -1,23 +1,32 @@
 function Pit (a,b,num){
 
-  var viking_one = a;
-  var viking_two = b;
+  var combatant_one = a;
+  var combatant_two = b;
   var turns = num;
 
-  function declareWinner(viking_one, viking_two){
-    return viking_one.health > viking_two.health ? viking_one.name + ' wins!' : viking_two.name + ' wins!' ;
+  function declareWinner(combatant_one, combatant_two){
+    return combatant_one.health > combatant_two.health ? combatant_one.name + ' wins!' : combatant_two.name + ' wins!' ;
   }
 
   this.startgame = function(){
-      while (viking_one.health > 20 && viking_two.health > 20 && turns !== 0) {
-        viking_one.doAttack(viking_two);
-        viking_two.doAttack(viking_one);
+      while (combatant_one.health > 20 && combatant_two.health > 20 && turns !== 0) {
+        combatant_one.doAttack(combatant_two);
+        combatant_two.doAttack(combatant_one);
         console.log('round: '+ turns);
-        console.log(viking_one.name +'is '+ viking_one.health + ' attacks ' + viking_two.name + ' is ' + viking_two.health);
+        console.log(combatant_one.name +'is '+ combatant_one.health + ' attacks ' + combatant_two.name + ' is ' + combatant_two.health);
         turns -=1;
       }
-      var winner = declareWinner(viking_one, viking_two);
+      var winner = declareWinner(combatant_one, combatant_two);
       console.log(winner);
+  };
+
+  this.deadCombat = function functionName() {
+    while (combatant_one.health > 0 && combatant_two.health > 0 ) {
+      combatant_one.doAttack(combatant_two);
+      combatant_two.doAttack(combatant_one);
+    }
+    var winner = declareWinner(combatant_one, combatant_two);
+    console.log(winner);
   };
 
 }
